8c3abcc692888d7113b34b058f86908a93338d5a
gxiang666 <1528357474@qq.com>
2019-04-06 17:54:54 +0800

    update

--- hadoop/src/hhbase/HbaseDao.java
+++ hadoop/src/hhbase/HbaseDao.java
@@ -17,15 +17,17 @@ import org.junit.Test;
 public class HbaseDao {
 
 	
-	@Test
+	@Test//插入数据
 	public void insertTest() throws Exception{
 		
 		Configuration conf =  HBaseConfiguration.create();
+		//配置zookeeper的地址
 		conf.set("hbase.zookeeper.quorum", "weekend05:2181,weekend06:2181,weekend07:2181");
-		
+		//表格，HBase插入数据只能一条条的插入
 		HTable nvshen = new HTable(conf, "nvshen");
-		
+		//行键
 		Put name = new Put(Bytes.toBytes("rk0001"));
+		//列族，key-value
 		name.add(Bytes.toBytes("base_info"), Bytes.toBytes("name"), Bytes.toBytes("angelababy"));
 		
 		Put age = new Put(Bytes.toBytes("rk0001"));
@@ -50,19 +52,24 @@ public class HbaseDao {
 		//负责创建表格
 		HBaseAdmin admin = new HBaseAdmin(conf);
 		
+		//表名
 		TableName name = TableName.valueOf("nvshen");
 		
-		
+		//表
 		HTableDescriptor desc = new HTableDescriptor(name);
 		
 		//列族
 		HColumnDescriptor base_info = new HColumnDescriptor("base_info");
 		HColumnDescriptor extra_info = new HColumnDescriptor("extra_info");
+		
+		//版本
 		base_info.setMaxVersions(5);
 		
+		//添加行键
 		desc.addFamily(base_info);
 		desc.addFamily(extra_info);
 		
+		//创建表
 		admin.createTable(desc);
 		
 		
--- hadoop/src/hive/PhoneNbrToArea.java
+++ hadoop/src/hive/PhoneNbrToArea.java
@@ -4,17 +4,19 @@ import java.util.HashMap;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
 
-
+//HIVE自定义函数
 public class PhoneNbrToArea extends UDF{
 
 	private static HashMap<String, String> areaMap = new HashMap<>();
+	
+	
 	static {
 		areaMap.put("1388", "beijing");
 		areaMap.put("1399", "tianjin");
 		areaMap.put("1366", "nanjing");
 	}
 	
-	//一定要用public修饰才能被hive调用
+	//一定要用public修饰才能被hive调用，执行的时候会被调用
 	public String evaluate(String pnb) {
 		
 		String result  = areaMap.get(pnb.substring(0,4))==null? (pnb+"    huoxing"):(pnb+"  "+areaMap.get(pnb.substring(0,4)));		
